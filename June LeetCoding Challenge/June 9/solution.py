d2adf8267facfb28e57e352d09cef7eae7ca7cb3
vairamuthu shanmugavel <vairamuthu.sv@zohocorp.com>
2020-06-11 08:04:45 +0530

    June 9 problem

--- June LeetCoding Challenge/June 9/solution.py
+++ June LeetCoding Challenge/June 9/solution.py
@@ -1,13 +1,16 @@
 from collections import deque
-
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) > len(t):
+            return False
+        if len(s) == 0:
+            return True
         q =  deque()
         for char in s:
             q.append(char)
         for char in t:
+            if len(q) == 0:
+                break
             if q[0] == char:
                 q.popleft()
         return len(q)==0
-        
-
